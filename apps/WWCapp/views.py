a6f8ec294c5b37737d3d6a217051ebf9c569b6a9
Sushma <sushma.m812@gmail.com>
2017-10-21 12:47:48 -0700

    sigin/register page

--- apps/WWCapp/views.py
+++ apps/WWCapp/views.py
@@ -1,12 +1,24 @@
 from django.shortcuts import render, HttpResponse, redirect
+from django.conf import settings
   # the index function is called when root is visited
-def index(request):
+def index(request): #signup/login
     response = "Hello, I am your first request!"
     return render(request, 'WWCapp/index.html')
 
-def victim(request):
+def victimrequests(request):
     content = {
             'first_name': "sushma"
 
         }
     return render(request, 'WWCapp/victimpage.html', content)
+
+def donations(request):
+
+    return render(request, 'WWCapp/donorpage.html')
+
+def register(request):
+    return redirect('/requests')
+
+def login(request):
+    print request.POST
+    return redirect('/donor')
