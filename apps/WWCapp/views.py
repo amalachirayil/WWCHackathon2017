b395f01a980dc23ec4ae68a9de8d8e15c6c9723a
Sushma <sushma.m812@gmail.com>
2017-10-21 11:01:42 -0700

    added index.html file

--- apps/WWCapp/views.py
+++ apps/WWCapp/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render, HttpResponse, redirect
   # the index function is called when root is visited
 def index(request):
     response = "Hello, I am your first request!"
-    return HttpResponse(response)
+    return render(request, 'WWCapp/index.html')
